91732354522b7a9e3d7c82b4bbd124537af90cfb
kkusumad <kalyakusumadi@gmail.com>
2020-11-20 16:16:16 +0800

    added another layer into field and turned values into objects with values and state for rendering purposes.

--- codes/game.js
+++ codes/game.js
@@ -1,11 +1,63 @@
 const field = [
-    [1, 2, 2, 1],
-    [1, "B", "B", 1],
-    [1, 2, 2, 1]
+    [
+        [{
+            value: 1,
+            state: "hidden"
+        }],
+        [{
+            value: 2,
+            state: "hidden"
+        }],
+        [{
+            value: 2,
+            state: "hidden"
+        }],
+        [{
+            value: 1,
+            state: "hidden"
+        }]
+    ],
+    [
+        [{
+            value: 1,
+            state: "hidden"
+        }],
+        [{
+            value: "B",
+            state: "hidden"
+        }],
+        [{
+            value: "B",
+            state: "hidden"
+        }],
+        [{
+            value: 1,
+            state: "hidden"
+        }]
+    ],
+    [
+        [{
+            value: 1,
+            state: "hidden"
+        }],
+        [{
+            value: 2,
+            state: "hidden"
+        }],
+        [{
+            value: 2,
+            state: "hidden"
+        }],
+        [{
+            value: 1,
+            state: "hidden"
+        }]
+    ]
 ];
 
 
 const clickCheck = (event) => {
+    changeState(event);
     const $clickVal = $(event.target).html();
     if ($clickVal === "B") {
         console.log("Bomb");
@@ -14,16 +66,28 @@ const clickCheck = (event) => {
     }
 };
 
+const changeState = (event) => {
+    $(event.target).removeClass();
+    console.log("changed state");
+};
 
-const setUp = () => {
+// const setUp = () => {
+
+// };
+
+const render = () => {
     for (let i = 0; i < field.length; i++) {
         const currentRow = field[i];
         const $field = $('#field');
         const $row = $('<div>').attr('id', "row-" + i);
         for (let j = 0; j < currentRow.length; j++) {
-            const currentCell = currentRow[j];
+            const currentCell = currentRow[j][0].value;
             const $col = $('<button>')
-                .attr('id', "col-" + j)
+                .attr({
+                    'id': "col-" + j
+                    ,
+                    'class': currentRow[j][0].state
+                })
                 .html(currentCell)
                 .on('click', clickCheck);
             $row.append($col);
@@ -32,13 +96,9 @@ const setUp = () => {
     }
 };
 
-const render = () => {
-
-};
-
 
 
 $(() => {
-    setUp();
+    // setUp();
     render();
 });
